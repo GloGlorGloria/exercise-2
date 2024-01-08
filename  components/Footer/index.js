0ee7ea872f4d9b91e20b16807618a1590c55a745
GloGlorGloria <gloriabcit@gmail.com>
2024-01-08 15:50:15 -0800

    New folders and files

---
+++  components/Footer/index.js
@@ -0,0 +1,8 @@
+import styles from './Footer.module.css'
+export default function Footer(){
+    return(
+        <footer className={styles.footer}>
+            <p>created by Gloria</p>
+        </footer>
+    )
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Exercise 1
+## Created by Gloria
+### In today's lesson, we learnt creating components in next.js, linking up pages and useState.
